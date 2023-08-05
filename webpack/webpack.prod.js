8af66071fd998cd4a2fb2abe55f51eb3cafb0537
Manuj Sankrit <msankrit8055@gmail.com>
2023-08-05 15:22:11 +0530

    Added performance hints matrix set to 500 KB

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -4,4 +4,12 @@ const commonConfiguration = require("./webpack.common.js");
 module.exports = merge(commonConfiguration, {
   mode: "production",
   target: "browserslist",
+  performance: {
+    maxAssetSize: 500000,
+    maxEntrypointSize: 500000,
+    hints: "error",
+    assetFilter: function (assetFilename) {
+      return !assetFilename.endsWith(".png");
+    },
+  },
 });
